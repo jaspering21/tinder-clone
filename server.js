1648c2ab11c0a755058e70735275ac79ecc87270
Jasper Jiao <jz630032@dal.ca>
2020-08-05 02:18:43 -0300

    update procfile

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const http = require('http');
-const app = require('./app');
-
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+const app = express();
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
-require('./src/App.js')(app);
-console.log(port)
-server.listen(port);
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.use(express.static(__dirname + '/build/'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Server is runing on port: ${port}`);
+});
