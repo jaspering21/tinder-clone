4c9584f3a6b3ab197e9fcb4bf54974091d48e781
Jasper Jiao <jz630032@dal.ca>
2020-08-05 02:30:19 -0300

    update procfile

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 app.use(express.static(__dirname + '/build/'));
 app.get('/', (req, res) => {
